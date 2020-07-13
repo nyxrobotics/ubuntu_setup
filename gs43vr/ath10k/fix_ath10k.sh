4d5c07018c528b1322d40b4dcbd8a4e980eb2b40
nyxrobotics <takaaki.numai01@g.softbank.co.jp>
2020-07-13 20:38:40 +0900

    Fix ath10k

---
+++ gs43vr/ath10k/fix_ath10k.sh
@@ -0,0 +1,16 @@
+# Reference : https://askubuntu.com/questions/607707/ath10k-installation
+# Please check dmesg with below commands
+# dmesg -l warn
+# dmesg -l err
+# dmesg |grep ath10k
+
+mkdir -p ~/setup/ath10k
+cd ~/setup/ath10k
+git clone https://github.com/kvalo/ath10k-firmware.git
+sudo cp -r ~/setup/ath10k/ath10k-firmware/* /lib/firmware/ath10k/
+cd /lib/firmware/ath10k/QCA6174/hw3.0
+sudo cp firmware-4.bin_WLAN.RM.2.0-00180-QCARMSWPZ-1 firmware-4.bin
+sudo cp firmware-4.bin_WLAN.RM.2.0-00180-QCARMSWPZ-1 firmware-6.bin
+
+#sudo chmod +x firmware-*
+#sudo chmod +x board*
